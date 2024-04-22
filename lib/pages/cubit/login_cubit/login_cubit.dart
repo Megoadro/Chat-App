cdd438fa8cf3f948aaf13b76577affbaa70f13ea
Megoadro <sayedkhalifa086@gmail.com>
2024-04-22 20:35:02 +0200

    create Login state and cubit

---
+++ lib/pages/cubit/login_cubit/login_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:cahtty/pages/cubit/login_cubit/login_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<loginState>{
+  LoginCubit() :super (loginInitialState());
+  
+
+}
---
+++ lib/pages/cubit/login_cubit/login_states.dart
@@ -0,0 +1,5 @@
+abstract class loginState{}
+class loginInitialState extends loginState{}
+class loginLoadinglState extends loginState{}
+class loginSuccessState extends loginState{}
+class loginFailuerState extends loginState{}
